28a47d97772659e6e9ab42af67512a1eb5ce22aa
an1kh <a.khulap@gmail.com>
2017-01-05 16:56:47 +0200

    Added sysfs control for on-time and off-time parameters.

--- Round2/a.khulap/coremod.c
+++ Round2/a.khulap/coremod.c
@@ -5,6 +5,7 @@
 #include <linux/leds.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/device.h>
 #include <linux/platform_device.h>
 #include <linux/property.h>
 #include <linux/timer.h>
@@ -13,10 +14,15 @@
 #define DRIVER_AUTHOR	"an1kh"
 #define DRIVER_DESC	"LED GPIO driver."
 #define DRIVER_NAME	"an1kh-leds-gpio"
+#define CLASS_NAME	DRIVER_NAME
 
 
+/* Forward declarations */
 static int gpio_led_probe(struct platform_device *pdev);
 static int gpio_led_remove(struct platform_device *pdev);
+static ssize_t control_show(struct class *class, struct class_attribute *attr, char *buf);
+static ssize_t control_store(struct class *class, struct class_attribute *attr, const char *buf, size_t count);
+
 
 static const struct of_device_id of_gpio_leds_match[] = {
 	{ .compatible = "an1kh,gpio-leds", },
@@ -44,7 +50,9 @@ struct gpio_led_data {
 };
 
 struct gpio_leds_table {
-	int	num_leds;
+	int			num_leds;
+	struct class		*control_class;
+	struct class_attribute	class_attr_control;
 	struct gpio_led_data	led_data[];
 };
 
@@ -78,7 +86,6 @@ static struct gpio_leds_table *gpio_leds_readparams(struct platform_device *pdev
 		curr_led->gpiod = devm_get_gpiod_from_child(dev, NULL, child);
 		if (IS_ERR(curr_led->gpiod)) {
 			dev_err(dev, "fail devm_get_gpiod_from_child()\n");
-			devm_kfree(dev, leds);		/* release memory */
 			return ERR_PTR(-ENODEV);
 		}
 
@@ -141,8 +148,45 @@ static int gpio_leds_release(struct platform_device *pdev, struct gpio_leds_tabl
 }
 
 
+static ssize_t control_show(struct class *class, struct class_attribute *attr, char *buf)
+{
+	int size, i;
+	struct gpio_leds_table *leds = container_of(attr, struct gpio_leds_table, class_attr_control);
+
+	size = sprintf(buf, "Total LEDs = %d\n", leds->num_leds);
+
+	for (i = 0; i < leds->num_leds; i++)
+		size += sprintf(buf + size,
+			"%d: \"%s\", on-time = %d, off-time = %d\n",
+			i, leds->led_data[i].label, leds->led_data[i].on_time, leds->led_data[i].off_time);
+
+	return size;
+}
+
+static ssize_t control_store(struct class *class, struct class_attribute *attr, const char *buf, size_t count)
+{
+	int num, on_time, off_time;
+	int ret;
+	struct gpio_leds_table *leds = container_of(attr, struct gpio_leds_table, class_attr_control);
+
+	/* Read parameters */
+	ret = sscanf(buf, "%d %d %d", &num, &on_time, &off_time);
+	/* Check parameters */
+	if (ret != 3)
+		return 0;
+	if (num >= leds->num_leds)
+		return 0;
+
+	leds->led_data[num].on_time = on_time;
+	leds->led_data[num].off_time = off_time;
+
+	return count;
+}
+
+
 static int gpio_led_probe(struct platform_device *pdev)
 {
+	int ret;
 	struct device *dev = &pdev->dev;
 	struct gpio_leds_table *leds;
 
@@ -152,8 +196,25 @@ static int gpio_led_probe(struct platform_device *pdev)
 	if (IS_ERR(leds))
 		return PTR_ERR(leds);
 
+	/* Create sysfs class to control LEDs */
+	leds->control_class = class_create(THIS_MODULE, CLASS_NAME);
+	if (IS_ERR(leds->control_class))
+		return PTR_ERR(leds->control_class);
+	/* Fill the attribute structure */
+	leds->class_attr_control.attr.name = __stringify(control);
+	leds->class_attr_control.attr.mode = VERIFY_OCTAL_PERMISSIONS(0664);
+	leds->class_attr_control.show = &control_show;
+	leds->class_attr_control.store = &control_store;
+	ret = class_create_file(leds->control_class, &(leds->class_attr_control));
+	if (IS_ERR_VALUE(ret)) {
+		class_remove_file(leds->control_class, &(leds->class_attr_control));
+		class_destroy(leds->control_class);
+		return ret;
+	}
+
 	gpio_leds_configure(pdev, leds);
 	platform_set_drvdata(pdev, leds);
+
 	return 0;
 }
 
@@ -165,9 +226,10 @@ static int gpio_led_remove(struct platform_device *pdev)
 	dev_info(dev, "dev_remove()");
 
 	leds = platform_get_drvdata(pdev);
-	gpio_leds_release(pdev, leds);
-	devm_kfree(dev, leds);
 
+	gpio_leds_release(pdev, leds);
+	class_remove_file(leds->control_class, &(leds->class_attr_control));
+	class_destroy(leds->control_class);
 	return 0;
 }
 
